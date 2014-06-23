5b0a234074118cb1c4b682407707598bae63e3db
marcokstephen <marcok.stephen@gmail.com>
2014-06-23 14:23:06 -0400

    Ready for release 6 (Version 1.8)

--- src/com/sm/daysuntilcards/ImportListAdapter.java
+++ src/com/sm/daysuntilcards/ImportListAdapter.java
@@ -2,9 +2,9 @@ package com.sm.daysuntilcards;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.List;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
@@ -20,19 +20,21 @@ import android.widget.CompoundButton.OnCheckedChangeListener;
 
 import com.sm.daysuntilcards.ImportCal.ViewHolder;
 
+@SuppressLint("SimpleDateFormat")
 class ImportListAdapter extends BaseAdapter {
-	
+
 	private List<CalEvent> calEventList;
 	private LayoutInflater myInflater;
 	private List<Boolean> checkedStatus;
-	
+	private Context ctxt;
+
 	public ImportListAdapter(Context c){
 		calEventList = ImportCal.calendarList;
 		myInflater = (LayoutInflater)c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		int size = calEventList.size();
 		checkedStatus = ImportCal.checkedStatus;
+		ctxt = c;
 	}
-	
+
 	@Override
 	public int getCount() {
 		return calEventList.size();
@@ -50,7 +52,7 @@ class ImportListAdapter extends BaseAdapter {
 
 	@Override
 	public View getView(final int position, View convertView, ViewGroup parent) {
-		
+
 		final CalEvent event = calEventList.get(position);
 		ViewHolder holder = null;
 		convertView = myInflater.inflate(R.layout.import_list_item_layout, null);
@@ -58,7 +60,7 @@ class ImportListAdapter extends BaseAdapter {
 		holder.event = (CheckBox) convertView.findViewById(R.id.calendar_event_checkbox);
 		holder.date = (TextView) convertView.findViewById(R.id.calendar_event_dateView);
 		convertView.setTag(holder);
-			
+
 		holder.event.setOnCheckedChangeListener(new OnCheckedChangeListener(){
 			@Override
 			public void onCheckedChanged(CompoundButton buttonView,
@@ -70,15 +72,15 @@ class ImportListAdapter extends BaseAdapter {
 				}
 			}
 		});
-		
+
 		SimpleDateFormat fromDateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 		SimpleDateFormat toDateFormat = new SimpleDateFormat("MMM d, yyyy  -  h:mm a");
-		//SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(ImportCal.this);
-		//boolean twentyFourHourClock = prefs.getBoolean("24hour", false);
-		//if (twentyFourHourClock){
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(ctxt);
+		boolean twentyFourHourClock = prefs.getBoolean("24hour", false);
+		if (twentyFourHourClock){
 			toDateFormat = new SimpleDateFormat("MMM d, yyyy - HH:mm");
-		//}
-		
+		}
+
 		Time time = new Time();
 		time.set(event.getDate());
 		int day = time.monthDay;
@@ -93,7 +95,7 @@ class ImportListAdapter extends BaseAdapter {
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
-		
+
 		holder.date.setText(outputTimeString);
 		holder.event.setChecked(checkedStatus.get(position));
 		holder.event.setText(event.getName());
